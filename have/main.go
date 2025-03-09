333f2f65624cd159995ae8898607b0b6e3e7201f
LimbergDV <limbergdelvalle@gmail.com>
2025-03-08 21:14:19 -0600

    add: adding relations of rents and cars

---
+++ have/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"have/entities"
+	"have/src"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+  // Cargar las variables de entorno
+  godotenv.Load()
+  rabbit := src.NewRabbitMQ()
+  
+  // Tratamiento de un mensaje
+  msgs := rabbit.GetMessages()
+  var forever chan struct{}
+
+  go func() {
+    for d := range msgs {
+        var rent entities.Rent
+        err := json.Unmarshal(d.Body, &rent)
+        if err != nil {
+            log.Printf("Error al decodificar el mensaje: %s", err)
+            continue
+        }
+        log.Printf(" [x] Prestamo recibido: id_customer=%d, id_car=%d, return_date=%s", rent.Id_Customer, rent.Id_Car, rent.Return_date_rent)
+        
+        if rent.Return_date_rent != "0000-00-00" {
+          src.RequestCarsFetchAPI(rent, "lend")
+        } else {
+		  src.RequestCarsFetchAPI(rent, "return")
+        } 
+    }
+}()
+
+  log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+  <-forever
+}
+
---
+++ have/src/cars_request_fetch.go
@@ -0,0 +1,33 @@
+package src
+
+import (
+	"bytes"
+	"encoding/json"
+	"have/entities"
+	"log"
+	"net/http"
+)
+
+func RequestCarsFetchAPI(rent entities.Rent, method string) {
+	URL := "http://localhost:8080/cars/" + method 
+	jsonBody, _ := json.Marshal(rent)
+
+	req, err := http.NewRequest(http.MethodPatch, URL, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		log.Fatalf("Error creando la petición PATCH: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error al ejecutar la petición PATCH: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("La petición PATCH devolvió el estado: %d", resp.StatusCode)
+	} else {
+		log.Println("Libro actualizado correctamente mediante PATCH")
+	}
+}
